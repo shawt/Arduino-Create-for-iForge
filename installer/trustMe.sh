86290e44488edcacc6f445e40b545ab996446eac
Trevor Shaw <shawt@genlrn.com>
2017-10-04 09:49:20 -0400

    correct commands for trusting certs and chmod

--- installer/trustMe.sh
+++ installer/trustMe.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-security add-trusted-cert -d -r trustAsRoot -k "cert.pem"
+security add-trusted-cert -d -r trustAsRoot -k "/Library/Keychains/System.keychain" "./cert.cer"
+security add-trusted-cert -d -r trustAsRoot -k "/Library/Keychains/System.keychain" "./ca.cert.cer"
 
+chmod 7555 "key.pem"
+chmod +x "acfi.plist"
